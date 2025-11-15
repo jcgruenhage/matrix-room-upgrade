eb3b31dc7027050de775c3c02267d2ec1d8ecdee
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2025-11-15 23:30:20 +0100

    feat: add re-run support for if an upgrade worked but invites had to be re-run, f.ex. due to rate limits

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,6 @@ async fn main() -> anyhow::Result<()> {
         .default_headers(headers)
         .build()?;
 
-
     let self_user_id_res = dbg!(
         dbg!(
             http_client
@@ -47,19 +46,27 @@ async fn main() -> anyhow::Result<()> {
     let self_user_id = dbg!(self_user_id_res["user_id"].as_str().unwrap().to_string());
 
     for room in config.rooms {
-        if http_client
+        let tombstone = http_client
             .get(format!(
                 "{}/_matrix/client/v3/rooms/{room}/state/m.room.tombstone/",
                 config.homeserver_url
             ))
             .send()
-            .await?
-            .status()
-            == StatusCode::OK
-        {
-            println!("Room {room} is already upgraded");
-            continue;
-        }
+            .await?;
+        let new_room_id = if tombstone.status() == StatusCode::OK {
+            println!("Room upgraded already, only transferring membership state");
+            let tombstone_content: serde_json::Value = dbg!(tombstone.json().await?);
+            Some(
+                tombstone_content
+                    .get("replacement_room")
+                    .context("no replacement_room, should be part of tombstone event")?
+                    .as_str()
+                    .context("no string wtf")?
+                    .to_string(),
+            )
+        } else {
+            None
+        };
 
         let old_members_res = http_client
             .get(&format!(
@@ -106,125 +113,132 @@ async fn main() -> anyhow::Result<()> {
         dbg!(&banned_members);
         dbg!(&joined_members);
 
-        // let new_room_id = if new_room_id.is_none() {
-        let mut state: HashMap<String, Value> = HashMap::new();
-        for event_type in &config.state_events_to_transfer {
+        let new_room_id = if new_room_id.is_none() {
+            let mut state: HashMap<String, Value> = HashMap::new();
+            for event_type in &config.state_events_to_transfer {
+                let res = http_client
+                    .get(format!(
+                        "{}/_matrix/client/v3/rooms/{room}/state/{event_type}/",
+                        config.homeserver_url
+                    ))
+                    .send()
+                    .await?;
+                if res.status() != StatusCode::OK {
+                    continue;
+                }
+                let mut val: Value = res.json().await?;
+                if event_type == "m.room.power_levels" {
+                    let map = val.as_object_mut().context("PL state is not an object")?;
+                    let users_default = match map.get("users_default") {
+                        Some(num) => num
+                            .as_number()
+                            .context("PL state key users_default is not a number")?
+                            .as_u64()
+                            .context("PL state key users_default is not a u64")?,
+                        None => 0,
+                    };
+                    let users = map
+                        .get_mut("users")
+                        .context("PL state does not contain users key")?
+                        .as_object_mut()
+                        .context("PL state key users is not an object")?;
+
+                    for (user_id, pl) in config.pl_overrides.iter() {
+                        if users_default == *pl {
+                            users.remove(user_id);
+                        } else {
+                            users.insert(user_id.to_string(), json!(*pl));
+                        }
+                        println!(
+                            "Overrode power level for user {user_id} in room {room} to be {pl}"
+                        )
+                    }
+
+                    if config.target_room_version >= 12 {
+                        users.remove(&self_user_id);
+                    }
+                }
+                state.insert(event_type.to_string(), val);
+            }
+            println!("New state for {room}: {state:#?}");
+
+            let txn_id = Uuid::new_v4();
             let res = http_client
-                .get(format!(
-                    "{}/_matrix/client/v3/rooms/{room}/state/{event_type}/",
+                .put(&format!(
+                    "{}/_matrix/client/v3/rooms/{room}/send/m.room.message/{txn_id}",
                     config.homeserver_url
+                ))
+                .json(&json!({
+                    "body": "Upgrading room, please stand by",
+                    "msgtype": "m.text"
+                }
                 ))
                 .send()
                 .await?;
-            if res.status() != StatusCode::OK {
-                continue;
-            }
-            let mut val: Value = res.json().await?;
-            if event_type == "m.room.power_levels" {
-                let map = val.as_object_mut().context("PL state is not an object")?;
-                let users_default = match map.get("users_default") {
-                    Some(num) => num
-                        .as_number()
-                        .context("PL state key users_default is not a number")?
-                        .as_u64()
-                        .context("PL state key users_default is not a u64")?,
-                    None => 0,
-                };
-                let users = map
-                    .get_mut("users")
-                    .context("PL state does not contain users key")?
-                    .as_object_mut()
-                    .context("PL state key users is not an object")?;
-                for (user_id, pl) in config.pl_overrides.iter() {
-                    if users_default == *pl {
-                        users.remove(user_id);
-                    } else {
-                        users.insert(user_id.to_string(), json!(*pl));
-                    }
-                    println!("Overrode power level for user {user_id} in room {room} to be {pl}")
-                }
+            let last_event_id = dbg!(dbg!(res).json::<Value>().await?)["event_id"]
+                .as_str()
+                .context("event_id is not a string")?
+                .to_string();
 
-                if config.target_room_version >= 12 {
-                    users.remove(&self_user_id);
-                }
-            }
-            state.insert(event_type.to_string(), val);
-        }
-        println!("New state for {room}: {state:#?}");
+            println!("Last event ID: {last_event_id}");
 
-        let txn_id = Uuid::new_v4();
-        let res = http_client
-            .put(&format!(
-                "{}/_matrix/client/v3/rooms/{room}/send/m.room.message/{txn_id}",
-                config.homeserver_url
-            ))
-            .json(&json!({
-                "body": "Upgrading room, please stand by",
-                "msgtype": "m.text"
-            }
-            ))
-            .send()
-            .await?;
-        let last_event_id = dbg!(dbg!(res).json::<Value>().await?)["event_id"]
-            .as_str()
-            .context("event_id is not a string")?
-            .to_string();
-
-        println!("Last event ID: {last_event_id}");
-
-        let power_level_content_override = dbg!(state.remove("m.room.power_levels").unwrap());
-        let initial_state: Vec<_> = dbg!(state
-            .into_iter()
-            .filter(|(event_type, _)| event_type != "m.room.power_levels")
-            .map(|(event_type, content)| {
-                json!({
-                    "content": content,
-                    "type": event_type,
+            let power_level_content_override = dbg!(state.remove("m.room.power_levels").unwrap());
+            let initial_state: Vec<_> = dbg!(state
+                .into_iter()
+                .filter(|(event_type, _)| event_type != "m.room.power_levels")
+                .map(|(event_type, content)| {
+                    json!({
+                        "content": content,
+                        "type": event_type,
+                    })
                 })
-            })
-            .collect());
+                .collect());
 
-        let target_room_version = format!("{}", config.target_room_version);
-        let new_room_res = dbg!(
+            let target_room_version = format!("{}", config.target_room_version);
+
+            let new_room_res = dbg!(
+                http_client
+                    .post(&format!(
+                        "{}/_matrix/client/v3/createRoom",
+                        config.homeserver_url
+                    ))
+                    .json(dbg!(&json!({
+                        "creation_content": {
+                            "predecessor": {
+                                "event_id": last_event_id,
+                                "room_id": room,
+                            },
+                        },
+                        "room_version": target_room_version,
+                        "power_level_content_override": power_level_content_override,
+                        "initial_state": initial_state,
+                    })))
+                    .send()
+                    .await
+            )?;
+            let new_room_body = dbg!(new_room_res.json::<Value>().await?);
+            let new_room_id = new_room_body["room_id"]
+                .as_str()
+                .context("room id is not a string")?;
+
             http_client
-                .post(&format!(
-                    "{}/_matrix/client/v3/createRoom",
+                .put(&format!(
+                    "{}/_matrix/client/v3/rooms/{room}/state/m.room.tombstone/",
                     config.homeserver_url
                 ))
-                .json(dbg!(&json!({
-                    "creation_content": {
-                        "predecessor": {
-                            "event_id": last_event_id,
-                            "room_id": room,
-                        },
-                    },
-                    "room_version": target_room_version,
-                    "power_level_content_override": power_level_content_override,
-                    "initial_state": initial_state,
-                })))
+                .json(&json!({
+                    "body": "This room has been replaced",
+                    "replacement_room": new_room_id,
+                }))
                 .send()
-                .await
-        )?;
-
-        let new_room_body = dbg!(new_room_res.json::<Value>().await?);
-
-        let new_room_id = new_room_body["room_id"]
-            .as_str()
-            .context("room id is not a string")?;
-
-        http_client
-            .put(&format!(
-                "{}/_matrix/client/v3/rooms/{room}/state/m.room.tombstone/",
-                config.homeserver_url
-            ))
-            .json(&json!({
-                "body": "This room has been replaced",
-                "replacement_room": new_room_id,
-            }))
-            .send()
-            .await?
-            .json::<Value>()
-            .await?;
+                .await?
+                .json::<Value>()
+                .await?;
+            new_room_id.to_string()
+        } else {
+            new_room_id
+                .context("we're in the else clause of a if is_none, it should be here really")?
+        };
 
         for (user_id, reason) in banned_members.iter() {
             dbg!(
